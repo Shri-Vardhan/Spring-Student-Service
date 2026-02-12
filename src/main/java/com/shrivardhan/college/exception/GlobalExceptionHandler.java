a4b7dbd131beddb93a6898303a2b5648197b2cfe
Shrivardhan <chshrivardhan@gmail.com>
2026-02-12 18:29:28 +0530

    Implementation of error code message and number

--- src/main/java/com/shrivardhan/college/exception/GlobalExceptionHandler.java
+++ src/main/java/com/shrivardhan/college/exception/GlobalExceptionHandler.java
@@ -1,16 +1,31 @@
 package com.shrivardhan.college.exception;
 
-import org.springframework.dao.DataIntegrityViolationException;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(StudentAlreadyExistsException.class)
-    public ResponseEntity<String> handleStudentExists(StudentAlreadyExistsException ex) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    public ResponseEntity<ErrorResponse> handleDuplicateUser(
+            StudentAlreadyExistsException ex,
+            HttpServletRequest request) {
+
+        ErrorResponse error = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.name(),
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(error);
     }
-    
+
 }
