bf339b43d413021216324dc33d0174dd4268c63e
Shrivardhan <chshrivardhan@gmail.com>
2026-02-15 16:11:54 +0530

    Completed functionality on Insert and Retrive records

--- src/main/java/com/shrivardhan/college/controller/StudentController.java
+++ src/main/java/com/shrivardhan/college/controller/StudentController.java
@@ -3,25 +3,25 @@ package com.shrivardhan.college.controller;
 import com.shrivardhan.college.model.Student;
 import com.shrivardhan.college.service.StudentService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/students")
-public class Studentcontroller {
+public class StudentController {
 
     private final StudentService studentService;
-    public Studentcontroller(StudentService studentService){
+
+    public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
 
 
     @GetMapping
-    public String getAllStudents(Model model){
+    public String getAllStudents(Model model) {
         List<Student> students = studentService.getStudents();
         model.addAttribute("students", students);
         return "students";   // refers to students.html
