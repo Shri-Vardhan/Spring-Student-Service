1e7d189e01d9ed7294c3fa5e48ca1ebae292fc75
Shrivardhan <chshrivardhan@gmail.com>
2026-03-31 19:45:53 +0530

    Fully Working module

--- src/main/java/com/shrivardhan/college/controller/WebViewStudent.java
+++ src/main/java/com/shrivardhan/college/controller/WebViewStudent.java
@@ -1,24 +1,3 @@
-/*
-package com.shrivardhan.college.controller;
-
-import com.shrivardhan.college.service.StudentService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class WebViewStudent {
-
-    @Autowired
-    private StudentService service;
-
-    @GetMapping("/web/viewStudents")
-    public String students(Model model) {
-        model.addAttribute("students", service.getStudents());
-        return "viewStudents";
-    }
-*/
 package com.shrivardhan.college.controller;
 
 import com.shrivardhan.college.model.Student;
